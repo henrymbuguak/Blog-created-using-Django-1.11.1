a45f1b6749e1a65e129664d7fafcaff746538b6d
henrymbuguak <henrymbuguak@gmail.com>
2017-06-28 20:12:46 +0300

    Slug implementation on blog post

--- posts/models.py
+++ posts/models.py
@@ -3,13 +3,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.core.urlresolvers import reverse
-
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 def upload_location(instance, filename):
     return "%s/%s" %(instance.id, filename)
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True )
     image = models.ImageField(upload_to=upload_location, null=True,blank=True, width_field="width_field", height_field="height_field")
     height_field = models.IntegerField(default=0)
     width_field = models.IntegerField(default=0)
@@ -26,3 +28,21 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-timestamp","-updated"]
+
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    queryset = Post.objects.filter(slug=slug).order_by("-id")
+    exists = queryset.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug, queryset.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+
+def pre_save_post_signal_receiver(sender,instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+pre_save.connect(pre_save_post_signal_receiver, sender=Post)
